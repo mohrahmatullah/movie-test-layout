3ab828afb373d40871c44f7839b053502879b3a5
rahmat <rahmetuloh@gmail.com>
2021-08-17 15:57:54 +0700

    make it better

--- application/models/Home.php
+++ application/models/Home.php
@@ -11,24 +11,11 @@ class Home extends CI_Model
 	    // $output = curl_exec($ch);
 	    // curl_close($ch);  
 
-	    $js = file_get_contents('https://imdb-api.com/en/API/Top250Movies/k_quzyc85y');
+	    // $js = file_get_contents('https://imdb-api.com/en/API/Top250Movies/k_quzyc85y');
+	    $js = file_get_contents('response.json');
   		$pjs = json_decode($js);
 
 		return $pjs->items;
-	}
-
-	public function get_details_data()
-	{
-	    // $ch = curl_init(); 
-	    // curl_setopt($ch, CURLOPT_URL, "https://www.kamibijak.com/api/post/category/all");
-	    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    // $output = curl_exec($ch);
-	    // curl_close($ch);  
-
-	    $js = file_get_contents('https://www.kamibijak.com/api/post/category/all');
-  		$pjs = json_decode($js);
-
-		return $pjs->data;
 	}
 
 }

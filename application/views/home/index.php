df5d0ce3b9a72bbdb225e93bd8695b30c15e59f3
rahmat <rahmetuloh@gmail.com>
2021-08-17 13:16:23 +0700

    first commit

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -27,53 +27,6 @@
           <?php } ?>
         <?php endforeach; ?>
       </div>
-      <?php foreach($database as $key => $value) : ?> 
-        <?php if($key < 10){ ?>
-          <!-- Modal -->
-          <div class="modal fade" id="exampleModal<?php echo $value->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content" style="background-color: #06121e; color: #abb7c4;">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel"><?php echo $value->title; ?></h5>
-                </div>
-                <div class="modal-body">
-                  <div class="container mt-5">
-                      <div class="d-flex justify-content-center row">
-                          <div class="align-content-center col-md-6">
-                              <div class="review-card p-2 bg-white rounded">
-                                  <div class="d-flex mt-0 p-2">
-                                      <img src="<?php echo $value->image; ?>">
-                                      <br><br>
-                                      <div class="ml-1 mt-2">
-                                          <h6 class="crop-text mb-0"><?php echo $value->title; ?></h6>
-                                          <div class="mt-0"><span class="cmpny-name">Tahun : <?php echo $value->year; ?></span></div>
-                                      </div>
-                                  </div>
-                                  <div class="px-4 rating-box mb-3">
-                                      <div class="border rounded">
-                                          <div class="text-center score py-2">
-                                              <div class="rating-out"><span class="get-rating">ImDbRating : <?php echo $value->imDbRating; ?></span></div>
-                                              <div><span>Crew : <?php echo $value->crew; ?></span></div>
-                                          </div>
-                                          <div class="d-flex justify-content-around align-items-center align-content-center border-top">
-                                              <div class="border-right px-2 text-center py-2"><span class="d-block font-weight-bold">Rank : </span><span><?php echo $value->rank; ?></span></div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #06121e;color: white;">Close</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        <?php } ?>
-      <?php endforeach; ?>
-
     </div>
   </div>
 </div>
@@ -102,57 +55,104 @@
               <?php } ?>
             <?php endforeach; ?>
           </div>
-
-          <?php foreach($database as $key => $value) : ?> 
-            <?php if($key > 9 && $key < 20){ ?>
-              <!-- Modal -->
-              <div class="modal fade" id="exampleModal<?php echo $value->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content" style="background-color: #06121e; color: #abb7c4;">
-                    <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel"><?php echo $value->title; ?></h5>
-                    </div>
-                    <div class="modal-body">
-                      <div class="container mt-5">
-                          <div class="d-flex justify-content-center row">
-                              <div class="align-content-center col-md-6">
-                                  <div class="review-card p-2 bg-white rounded">
-                                      <div class="d-flex mt-0 p-2">
-                                          <img src="<?php echo $value->image; ?>">
-                                          <br><br>
-                                          <div class="ml-1 mt-2">
-                                              <h6 class="crop-text mb-0"><?php echo $value->title; ?></h6>
-                                              <div class="mt-0"><span class="cmpny-name">Tahun : <?php echo $value->year; ?></span></div>
-                                          </div>
-                                      </div>
-                                      <div class="px-4 rating-box mb-3">
-                                          <div class="border rounded">
-                                              <div class="text-center score py-2">
-                                                  <div class="rating-out"><span class="get-rating">ImDbRating : <?php echo $value->imDbRating; ?></span></div>
-                                                  <div><span>Crew : <?php echo $value->crew; ?></span></div>
-                                              </div>
-                                              <div class="d-flex justify-content-around align-items-center align-content-center border-top">
-                                                  <div class="border-right px-2 text-center py-2"><span class="d-block font-weight-bold">Rank : </span><span><?php echo $value->rank; ?></span></div>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #06121e;color: white;">Close</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            <?php } ?>
-          <?php endforeach; ?>
         </div>   
       </div>
     </div>
   </div>
 </div>
 
+<?php foreach($database as $key => $value) : ?> 
+  <?php if($key < 10){ ?>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal<?php echo $value->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content" style="background-color: #06121e; color: #abb7c4;">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"><?php echo $value->fullTitle; ?></h5>
+          </div>
+          <div class="modal-body">
+            <div class="container mt-5">
+                <div class="d-flex justify-content-center row">
+                    <div class="align-content-center col-md-6">
+                        <div class="review-card p-2 bg-white rounded">
+                            <div class="d-flex mt-0 p-2">
+                                <img src="<?php echo $value->image; ?>">
+                                <br><br>
+                                <div class="ml-1 mt-2">
+                                    <h6 class="crop-text mb-0"><?php echo $value->title; ?></h6>
+                                    <div class="mt-0"><span class="cmpny-name">Tahun : <?php echo $value->year; ?></span></div>
+                                </div>
+                            </div>
+                            <div class="px-4 rating-box mb-3">
+                                <div class="border rounded">
+                                    <div class="score py-2">
+                                        <div class="rating-out"><span class="get-rating">Rating : <?php echo $value->imDbRating; ?></span></div>
+                                        <div><span>Crew : <?php echo $value->crew; ?></span></div>
+                                    </div>
+                                    <div class="d-flex justify-content-around align-items-center align-content-center border-top">
+                                        <div class="border-right px-2 py-2"><span class="d-block font-weight-bold">Rank : </span><span><?php echo $value->rank; ?></span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #06121e;color: white;">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php } ?>
+<?php endforeach; ?>
+
+
+<?php foreach($database as $key => $value) : ?> 
+  <?php if($key > 9 && $key < 20){ ?>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal<?php echo $value->id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content" style="background-color: #06121e; color: #abb7c4;">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel"><?php echo $value->fullTitle; ?></h5>
+          </div>
+          <div class="modal-body">
+            <div class="container mt-5">
+                <div class="d-flex justify-content-center row">
+                    <div class="align-content-center col-md-6">
+                        <div class="review-card p-2 bg-white rounded">
+                            <div class="d-flex mt-0 p-2">
+                                <img src="<?php echo $value->image; ?>">
+                                <br><br>
+                                <div class="ml-1 mt-2">
+                                    <h6 class="crop-text mb-0"><?php echo $value->title; ?></h6>
+                                    <div class="mt-0"><span class="cmpny-name">Tahun : <?php echo $value->year; ?></span></div>
+                                </div>
+                            </div>
+                            <div class="px-4 rating-box mb-3">
+                                <div class="border rounded">
+                                    <div class="score py-2">
+                                        <div class="rating-out"><span class="get-rating">Rating : <?php echo $value->imDbRating; ?></span></div>
+                                        <div><span>Crew : <?php echo $value->crew; ?></span></div>
+                                    </div>
+                                    <div class="d-flex justify-content-around align-items-center align-content-center border-top">
+                                        <div class="border-right px-2 py-2"><span class="d-block font-weight-bold">Rank : </span><span><?php echo $value->rank; ?></span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #06121e;color: white;">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php } ?>
+<?php endforeach; ?>
 <?php $this->load->view("_partials/footer.php") ?>
